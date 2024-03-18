108dbf855995373a64f6d91dd2744abcf6693a92
George Donnelly <me@georgedonnelly.com>
2024-03-17 21:04:52 -0500

    update accountForm formatting

--- components/contracts/accountForm.tsx
+++ components/contracts/accountForm.tsx
@@ -32,7 +32,7 @@ export const AccountForm: React.FC<AccountFormProps> = ({ onSubmit }) => {
   };
 
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center mb-4">
       <form
         onSubmit={handleSubmit}
         className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full max-w-md"
@@ -97,9 +97,9 @@ export const AccountForm: React.FC<AccountFormProps> = ({ onSubmit }) => {
         <div className="flex items-center justify-center">
           <button
             type="submit"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
           >
-            Submit
+            Register Account
           </button>
         </div>
       </form>
