4e6a0cd78fa38f750737bbca4e64be8d326ea225
George Donnelly <me@georgedonnelly.com>
2025-04-22 19:32:53 +0000

    add fee flag

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,4 +4,5 @@ export const config = {
   dynamicSdkId: import.meta.env.VITE_DYNAMIC_ENV_ID,
   celoRpcUrl: import.meta.env.VITE_CELO_RPC_URL,
   contractAddress: import.meta.env.VITE_CONTRACT_ADDRESS,
+  usdcAddressAlfajores: import.meta.env.VITE_USDC_ADDRESS_ALFAJORES, // Add USDC address
 };
