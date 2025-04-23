f31d443fdf0f1a9a582d79306a55b0d6184c0e71
George Donnelly <me@georgedonnelly.com>
2025-04-23 23:31:43 +0000

    update App for change of function name in Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import { useState, useEffect } from "react";
-import { useDynamicContext, getAuthToken } from "@dynamic-labs/sdk-react-core";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./Header";
+import { useState, useEffect } from 'react';
+import { useDynamicContext, getAuthToken } from '@dynamic-labs/sdk-react-core';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './Header';
 import Footer from './Footer';
-import OffersPage from "./Home";
-import CreateOfferPage from "./CreateOfferPage";
-import AccountPage from "./AccountPage";
-import { getAccount, setAuthToken } from "./api";
-import { Account } from "./api";
-import Container from "./components/Container";
+import HomePage from './Home';
+import CreateOfferPage from './CreateOfferPage';
+import AccountPage from './AccountPage';
+import { getAccount, setAuthToken } from './api';
+import { Account } from './api';
+import Container from './components/Container';
 
-import MyOffersPage from "./MyOffersPage";
-import MyTradesPage from "./MyTradesPage";
-import MyEscrowsPage from "./MyEscrowsPage";
-import OfferDetailPage from "./OfferDetailPage";
-import EditOfferPage from "./EditOfferPage";
+import MyOffersPage from './MyOffersPage';
+import MyTradesPage from './MyTradesPage';
+import MyEscrowsPage from './MyEscrowsPage';
+import OfferDetailPage from './OfferDetailPage';
+import EditOfferPage from './EditOfferPage';
 import TradePage from './TradePage';
 import { Toaster } from '@/components/ui/sonner'; // Import Toaster
 
@@ -27,17 +27,17 @@ function App() {
       const token = getAuthToken();
       if (token) {
         // console.log("JWT Token:", token);
-        console.log("JWT Token found after wallet connect!");
+        console.log('JWT Token found after wallet connect!');
         setAuthToken(token);
       } else {
-        console.error("No JWT token found after wallet connect!");
+        console.error('No JWT token found after wallet connect!');
       }
       const getUserData = async () => {
         try {
           const response = await getAccount();
           setAccount(response.data);
         } catch (err) {
-          console.error("Failed to fetch account:", err);
+          console.error('Failed to fetch account:', err);
         }
       };
       getUserData();
@@ -51,11 +51,14 @@ function App() {
         <main className="main-content">
           <Container>
             <Routes>
-              <Route path="/account" element={<AccountPage account={account} setAccount={setAccount} />} />
-              <Route path="/" element={<OffersPage />} />
+              <Route
+                path="/account"
+                element={<AccountPage account={account} setAccount={setAccount} />}
+              />
+              <Route path="/" element={<HomePage />} />
               <Route path="/create-offer" element={<CreateOfferPage account={account} />} />
-              <Route path="/offers" element={<MyOffersPage account={account} /> } />
-              <Route path="/trades" element={<MyTradesPage account={account} /> } />
+              <Route path="/offers" element={<MyOffersPage account={account} />} />
+              <Route path="/trades" element={<MyTradesPage account={account} />} />
               <Route path="/escrows" element={<MyEscrowsPage account={account} />} />
               <Route path="/offer/:id" element={<OfferDetailPage />} />
               <Route path="/edit-offer/:id" element={<EditOfferPage />} />
@@ -63,10 +66,10 @@ function App() {
             </Routes>
           </Container>
         </main>
-       <Footer />
-       <Toaster richColors /> {/* Add Toaster here */}
-     </div>
-   </Router>
+        <Footer />
+        <Toaster richColors /> {/* Add Toaster here */}
+      </div>
+    </Router>
   );
 }
 
