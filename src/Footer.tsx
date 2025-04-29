89bb396f201920dc5c2905eb1e243ad364e0b6e4
George Donnelly <me@georgedonnelly.com>
2025-04-29 15:33:20 +0000

    clean up footer formatting

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -128,6 +128,32 @@ export const Footer: React.FC = () => {
                   <path d="M11.944 0A12 12 0 0 0 0 12a12 12 0 0 0 12 12 12 12 0 0 0 12-12A12 12 0 0 0 12 0a12 12 0 0 0-.056 0zm4.962 7.224c.1-.002.321.023.465.14a.506.506 0 0 1 .171.325c.016.093.036.306.02.472-.18 1.898-.962 6.502-1.36 8.627-.168.9-.499 1.201-.82 1.23-.696.065-1.225-.46-1.9-.902-1.056-.693-1.653-1.124-2.678-1.8-1.185-.78-.417-1.21.258-1.91.177-.184 3.247-2.977 3.307-3.23.007-.032.014-.15-.056-.212s-.174-.041-.249-.024c-.106.024-1.793 1.14-5.061 3.345-.48.33-.913.49-1.302.48-.428-.008-1.252-.241-1.865-.44-.752-.245-1.349-.374-1.297-.789.027-.216.325-.437.893-.663 3.498-1.524 5.83-2.529 6.998-3.014 3.332-1.386 4.025-1.627 4.476-1.635z" />
                 </svg>
               </a>
+              <a
+                href="mailto:hello@panmoni.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Email"
+                className="flex items-center"
+              >
+                <svg
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="text-primary-700 hover:text-primary-800 w-6 h-6"
+                >
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                  <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                  <g id="SVGRepo_iconCarrier">
+                    {' '}
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M3.75 5.25L3 6V18L3.75 18.75H20.25L21 18V6L20.25 5.25H3.75ZM4.5 7.6955V17.25H19.5V7.69525L11.9999 14.5136L4.5 7.6955ZM18.3099 6.75H5.68986L11.9999 12.4864L18.3099 6.75Z"
+                      fill="#080341"
+                    ></path>{' '}
+                  </g>
+                </svg>
+              </a>
             </div>
           </div>
         </div>
@@ -163,14 +189,14 @@ export const Footer: React.FC = () => {
             .
           </p>
           <p>
-            &copy; {new Date().getFullYear()} A{' '}
+            &copy; 2023-{new Date().getFullYear()} A{' '}
             <a className="panmoni-link" href="https://panmoni.com" target="_blank" rel="noopener">
+              Panmoni
               <img
                 src="/panmoni.svg"
                 alt="Panmoni Logo"
-                className="w-4 h-4 inline mr-1 align-middle"
+                className="w-4 h-4 inline ml-1 align-middle"
               />
-              Panmoni
             </a>{' '}
             project
           </p>
