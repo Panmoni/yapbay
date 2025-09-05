e5ae676f29a326ef97f2b9734eb846a4063ac5dd
George Donnelly <me@georgedonnelly.com>
2025-09-05 19:07:33 +0000

    update version

--- src/utils/version.ts
+++ src/utils/version.ts
@@ -1,9 +1,9 @@
 // This file is auto-generated. Do not edit manually.
 export const versionInfo = {
   "version": "0.1.2",
-  "gitCommitHash": "2666964",
-  "gitCommitDate": "2025-06-03T20:22:27+00:00",
+  "gitCommitHash": "3ce5ccc",
+  "gitCommitDate": "2025-09-05T18:07:26+00:00",
   "gitBranch": "main",
-  "buildDate": "2025-06-03T20:25:33.048Z",
+  "buildDate": "2025-09-05T18:11:11.981Z",
   "isDirty": true
 };
